d8fa05306f18292c4e8ef8bdc68f4af263e99416
erfinfeluzy <erfinfeluzy@36.86.63.182>
2019-03-03 13:37:56 +0700

    initial

---
+++ spring/src/main/java/com/erfinfeluzy/training/model/Customer.java
@@ -0,0 +1,19 @@
+package com.erfinfeluzy.training.model;
+
+import java.util.Date;
+
+
+
+public class Customer {
+
+	private String username;
+	
+	private String firstName;
+	
+	private String lastName;
+	
+	private Date birthdate;
+	
+	
+	
+}
