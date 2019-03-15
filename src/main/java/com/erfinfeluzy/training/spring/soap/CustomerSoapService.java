0ffe7dca36b643f04959a469d48e6167a16a72d5
Wahyu Sudrajat <wahyusudrajat07@gmail.com>
2019-03-16 01:26:23 +0700

    Add config to define SOAP consumer

--- src/main/java/com/erfinfeluzy/training/spring/soap/CustomerSoapService.java
+++ src/main/java/com/erfinfeluzy/training/spring/soap/CustomerSoapService.java
@@ -6,10 +6,9 @@ import javax.jws.WebService;
 
 import com.erfinfeluzy.training.spring.model.Customer;
 
-@WebService(serviceName = "InfoService")
+@WebService
 public interface CustomerSoapService {
 
-	
 	List<Customer> getAllCustomer();
 	
 	List<Customer> getCustomerByFirstname(String firstname);
